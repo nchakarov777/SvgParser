9cdd6921c2d93d39d52b45a5b154e52f800725c3
Nikito <nikito.chak@gmail.com>
2017-05-31 20:08:28 +0300

    Implement the file reading functionality.

--- SvgParser/Controller.cpp
+++ SvgParser/Controller.cpp
@@ -1,11 +1,24 @@
 #include "Controller.h"
+#include<fstream>
 
 using namespace std;
 
-Controller::Controller(){
+Controller::Controller()
+{
 
 }
 
-void Controller::openAndReadSvgFile(string fileName){
-    cout<<fileName;
+void Controller::openAndReadSvgFile(string fileName)
+{
+    ifstream svg;
+    //Used c_str because of https://stackoverflow.com/questions/10966446/passing-a-string-to-file-open
+    svg.open(fileName.c_str());
+    if (svg.is_open())
+    {
+        cout<<"inside"<<endl;
+        for(string line; getline(svg, line);){
+            cout<<line<<endl;
+        }
+    }
+    svg.close();
 }
